340191aa248fe4ecd42e9f89ff055f95b4cb0686
EugeneAlWin <evgenijgoncarevic@gmail.com>
2023-11-21 01:50:58 +0300

    config update

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,5 +1,8 @@
 import 'dotenv/config'
+import * as process from 'process'
 
 export const CONFIG = {
 	PORT: process.env.PORT || 3000,
+	JWT_ACCESS: process.env.JWT_ACCESS || 'AcCeSs',
+	JWT_REFRESH: process.env.JWT_REFRESH || 'ReFrEsH',
 }
